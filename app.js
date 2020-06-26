65f5548f9403d0a2f0524cc389992c0e64eb4b38
Dereck Goolsby-Bearsong <dgoolsby-bearsong@mii.com>
2020-06-26 14:21:01 -0600

    Completion of wiki API

--- app.js
+++ app.js
@@ -22,6 +22,87 @@ const articleSchema = new mongoose.Schema({
 });
 const Article = new mongoose.model("Article", articleSchema);
 
+// handles all the "all articles" requests
+app.route("/articles")
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles){
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      };
+    });
+  })
+  .post(function(req, res){
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err){
+      if (!err) {
+        res.send("Successfully added a new article.")
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function(req,res){
+    Article.deleteMany(function(err){
+      if(!err){
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+// Handles the individual articles requests
+app.route("/articles/:articleTitle")
+  .get(function(req, res) {
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No Articles with that title were found");
+      }
+    });
+})
+.put(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if (!err) {
+        res.send("Article updated successfully!");
+      }
+    });
+})
+.patch(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+.delete(function(req, res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully deleted article.");
+      } else {
+        res.send(err);
+      }
+    });
+});
+
 // Assigns port to listen to when fired up
 app.listen(3000, function(){
   console.log("The server has been started on port 3000");
